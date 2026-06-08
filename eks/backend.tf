697a32a01eb6fa6a3ee886ce14a7337335280a48
p-kn2 <piyush.dev0325@gmail.com>
2026-06-08 21:16:29 +0530

    Comment out dynamodb_table in backend.tf
    Comment out the dynamodb_table configuration for clarity.

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,8 @@ terraform {
     bucket         = "dev-tej-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    // dynamodb_table = "Lock-Files"
+    use_lockfile = true
     encrypt        = true
   }
 }
